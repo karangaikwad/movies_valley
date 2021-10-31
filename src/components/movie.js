f394098f26405699b74f2fc55ca267c369bed1fe
unknown <karanrg3005@gmail.com>
2021-10-31 12:17:43 +0530

    first commit

---
+++ src/components/movie.js
@@ -0,0 +1,19 @@
+function Movie(props) {
+  return (
+    <>
+      {/* {props.movies.results.map((movie) => {
+        return <div key={movie.id}>
+
+
+
+        </div>;
+      })} */}
+      <div>
+        {/* <img src={"https://image.tmdb.org/t/p/w500" + props.movies.results[2].poster_path}/>
+        <h1>{props.movies.results[2].title}</h1> */}
+      
+      </div>
+    </>
+  );
+}
+export default Movie;
