5e5fea56874bac8e6dc30ec6cae0e290b6b9b41b
abhidn <abhidnyanjadhavca2018@gmail.com>
2021-10-31 18:51:37 +0530

    5th commit

--- src/components/movie.js
+++ src/components/movie.js
@@ -1,15 +1,23 @@
 function Movie(props) {
   return (
-    <div>
+    <div class="outer-wrapper">
+  
+  
+
+    <div class="wrapper" >
+    
+     
+
       {props.movies.results.map((movie) => {
         return (
-          <img
+          <img class="zoom"
             key={movie.id}
             className="img1"
             src={"https://image.tmdb.org/t/p/w500" + movie.poster_path}
           />
         );
       })}
+    </div>
     </div>
   );
 }
