03587f2fa1c52c410caf53fb84119ad6327bee99
unknown <karanrg3005@gmail.com>
2021-11-01 12:55:31 +0530

    6th commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import Movie from "./components/movie"
-import {useEffect,useState} from "react";
+import Movie from "./components/movie";
+import { useEffect, useState } from "react";
 function App() {
   // const upi_key = "73383b0925b8752ef9ebe0b1e141ec78";
-  const url = "https://api.themoviedb.org/3/discover/movie?api_key=73383b0925b8752ef9ebe0b1e141ec78&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate";
-  const [movies,setMovies] = useState(null);
+  const url =
+    "https://api.themoviedb.org/3/discover/movie?api_key=73383b0925b8752ef9ebe0b1e141ec78&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate";
+  const [movies, setMovies] = useState(null);
   useEffect(() => {
     getData();
     async function getData() {
       const response = await fetch(url);
       const data = await response.json();
-  
-      setMovies(data) ;
+
+      setMovies(data);
     }
-    
-  },[]);
+  }, []);
   console.log(movies);
   return (
     <>
-    <div>
-      {movies && <Movie movies = {movies}/>}
-    </div>
+      <div>{movies && <Movie movies={movies} />}</div>
     </>
   );
 }
--- src/components/movie.js
+++ src/components/movie.js
@@ -1,23 +1,17 @@
 function Movie(props) {
   return (
     <div class="outer-wrapper">
-  
-  
-
-    <div class="wrapper" >
-    
-     
-
-      {props.movies.results.map((movie) => {
-        return (
-          <img class="zoom"
-            key={movie.id}
-            className="img1"
-            src={"https://image.tmdb.org/t/p/w500" + movie.poster_path}
-          />
-        );
-      })}
-    </div>
+      <div class="wrapper">
+        {props.movies.results.map((movie) => {
+          return (
+            <img
+              key={movie.id}
+              className="img1 zoom"
+              src={"https://image.tmdb.org/t/p/w500" + movie.poster_path}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
