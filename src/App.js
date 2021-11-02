80f7a12fe21313aaa2c50438dae1484e7aa9ca17
unknown <karanrg3005@gmail.com>
2021-11-02 10:59:11 +0530

    7th commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   const url =
     "https://api.themoviedb.org/3/discover/movie?api_key=73383b0925b8752ef9ebe0b1e141ec78&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate";
   const [movies, setMovies] = useState(null);
+  const [movieid, setMovieid] = useState(null);
+  const [element, setElement] = useState(null);
   useEffect(() => {
     getData();
     async function getData() {
@@ -13,11 +15,25 @@ function App() {
 
       setMovies(data);
     }
-  }, []);
+    change();
+    function change() {
+      if (movieid !== null) {
+        const ele = <div>Hello {movieid}</div>;
+        setElement(ele);
+      }
+    }
+  }, [movieid]);
+  function changem(id) {
+    setMovieid(id);
+  }
+  console.log(element);
   console.log(movies);
   return (
     <>
-      <div>{movies && <Movie movies={movies} />}</div>
+      <div>
+        {movies && <Movie movies={movies} change={changem} />}
+        {element && <h1 className="color1">{element}</h1>}
+      </div>
     </>
   );
 }
--- src/components/movie.js
+++ src/components/movie.js
@@ -1,10 +1,17 @@
 function Movie(props) {
+  function handlechange(movieid) {
+    // console.log(movieid);
+    props.change(movieid);
+  }
   return (
-    <div class="outer-wrapper">
-      <div class="wrapper">
+    <div className="outer-wrapper">
+      <div className="wrapper">
         {props.movies.results.map((movie) => {
           return (
             <img
+              onClick={() => {
+                handlechange(movie.id);
+              }}
               key={movie.id}
               className="img1 zoom"
               src={"https://image.tmdb.org/t/p/w500" + movie.poster_path}
